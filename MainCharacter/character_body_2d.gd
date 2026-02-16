e6ecc525d0207434df5b40dc93a2b6c6f389adec
Jamot32 <syoung3323@gmail.com>
2026-02-16 15:29:55 -0800

    movement + dash + cooldown
    movement + dash + cooldown

---
+++ MainCharacter/character_body_2d.gd
@@ -0,0 +1,93 @@
+extends CharacterBody2D
+
+@export var speed = 300.0
+@export var jump_velocity = -450.0
+@export var wall_slide_speed = 150.0
+@export var wall_jump_force = 400.0
+@export var dash_speed = 1200.0
+@export var dash_duration = 0.15
+@export var dash_cooldown = 3.0
+
+@export_group("Dash UI")
+@export var indicator_anchor: Control.LayoutPreset = Control.PRESET_BOTTOM_RIGHT
+@export var indicator_margin: int = 100
+@export var indicator_offset: Vector2 = Vector2(-20, -20)
+@export var indicator_scale: Vector2 = Vector2(0.5, 0.5)
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var facing_direction = 1.0
+var is_dashing = false
+var dash_timer = 0.0
+var dash_cooldown_timer = 0.0
+
+@onready var dash_indicator = $CanvasLayer/DashIndicator
+
+func _ready():
+	if not has_node("CanvasLayer"):
+		var cl = CanvasLayer.new()
+		add_child(cl)
+		var progress = TextureProgressBar.new()
+		progress.name = "DashIndicator"
+		progress.fill_mode = TextureProgressBar.FILL_CLOCKWISE
+		progress.step = 0.01
+		progress.max_value = dash_cooldown
+		progress.value = dash_cooldown
+		progress.texture_under = load("res://Assets/icon.svg")
+		progress.texture_progress = load("res://Assets/icon.svg")
+		progress.tint_under = Color(0.2, 0.2, 0.2, 0.5)
+		progress.tint_progress = Color(1, 1, 1, 1)
+		progress.scale = indicator_scale
+		progress.set_anchors_and_offsets_preset(indicator_anchor, Control.PRESET_MODE_KEEP_SIZE, indicator_margin)
+		progress.position += indicator_offset
+		cl.add_child(progress)
+		dash_indicator = progress
+
+func _physics_process(delta):
+	if dash_cooldown_timer > 0:
+		dash_cooldown_timer -= delta
+		if dash_indicator:
+			dash_indicator.value = dash_cooldown - dash_cooldown_timer
+	if is_dashing:
+		dash_timer -= delta
+		if dash_timer <= 0:
+			is_dashing = false
+		move_and_slide()
+		return
+
+	if not is_on_floor():
+		if is_on_wall_only() and velocity.y > 0:
+			velocity.y += (gravity * 0.5) * delta
+			velocity.y = min(velocity.y, wall_slide_speed)
+		else:
+			velocity.y += gravity * delta
+
+	var jump_attempt = Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")
+
+	if jump_attempt:
+		if is_on_floor():
+			velocity.y = jump_velocity
+		elif is_on_wall_only():
+			velocity.y = jump_velocity
+			velocity.x = get_wall_normal().x * wall_jump_force
+
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x = direction * speed
+		facing_direction = sign(direction)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	if Input.is_action_just_pressed("dash") and dash_cooldown_timer <= 0:
+		start_dash()
+
+	move_and_slide()
+
+func start_dash():
+	is_dashing = true
+	dash_timer = dash_duration
+	dash_cooldown_timer = dash_cooldown
+	velocity.x = facing_direction * dash_speed
+	velocity.y = 0
+	if dash_indicator:
+		dash_indicator.value = 0
